628a629b3d959b615febd66e80b0369a42459fa2
eddiefriedman1774 <eddiefriedman1@gmail.com>
2023-04-30 23:32:28 +0300

    added toggleoptions

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleDropDown(){
+    let dropdownContent = document.getElementById("dropdown-content")
+    if(dropdownContent.style.display=="none"){
+        document.getElementById("dropdown-content").style.display="block"
+    } else{
+        document.getElementById("dropdown-content").style.display="none"
+    }
+}
+
+function bodyLoad (){
+    document.getElementById("dropdown").addEventListener("click",toggleDropDown)
+}
